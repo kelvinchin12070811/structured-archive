1d5df30ff4316941d075ec0e308c7fbf18c88171
Kelvin Chin <chinkaiwen@yahoo.com>
2023-01-31 17:22:39 +0800

    Initialized repository

---
+++ structured-archive/structured-archive.cpp
@@ -0,0 +1,12 @@
+﻿// structured-archive.cpp : Defines the entry point for the application.
+//
+
+#include "structured-archive.hpp"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
